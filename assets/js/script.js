0c825ecb21fd307c7dbfd962a5bc93c74494d2eb
Scott Nichols <s21nichols@hotmail.com>
2021-12-17 01:00:20 -0700

    displayed current weather to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,15 @@ var weather = {
     var { temp, humidity } = data.main;
     var { speed } = data.wind;
     console.log(name,icon,description,temp,humidity,speed);
-    document.querySelector("city")
+    document.querySelector(".city").innerText = name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+    document.querySelector(".temp").innerText = temp + " F";
+    document.querySelector(".humidity").innerText = humidity;
+    document.querySelector(".wind").innerText = speed;
   }
 };
+
+var timeDisplay = moment();
+var timeContainer = $("#currentDay");
+timeContainer.append(timeDisplay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
