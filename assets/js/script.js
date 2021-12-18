e60bd2e62512dbb68c042746c0744845aabc3e84
Scott Nichols <s21nichols@hotmail.com>
2021-12-17 19:55:20 -0700

    added event listener for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,26 @@ var weather = {
     document.querySelector(".city").innerText = name;
     document.querySelector(".description").innerText = description;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
-    document.querySelector(".temp").innerText = temp + " F";
-    document.querySelector(".humidity").innerText = humidity;
-    document.querySelector(".wind").innerText = speed;
-  }
+    document.querySelector(".temp").innerText = temp + "F";
+    document.querySelector(".humidity").innerText = humidity +"%";
+    document.querySelector(".wind").innerText = speed +"mph";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
 var timeDisplay = moment();
 var timeContainer = $("#currentDay");
